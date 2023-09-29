68adaa1a1668186fbef2aca4dbc59fa1917defb8
pushpamohankumar <heshikagowda5@gmail.com>
2023-09-29 07:27:50 +0530

    NewAddlyDoctorModule

---
+++ src/test/java/com/hms/Admin/AddDoctorModuleDemo.java
@@ -0,0 +1,10 @@
+package com.hms.Admin;
+
+import org.testng.annotations.Test;
+
+public class AddDoctorModuleDemo {
+@Test
+public void addDoctor() {
+	System.out.println("Doctor Module Demo");
+}
+}
